5deabbb1e2e493601c77c2f61d361381fd1734fa
yuzhiyizhan <liangritian18666@163.com>
2023-02-13 00:28:02 +0800

    优化UI

--- js/chagpt.js
+++ js/chagpt.js
@@ -1,13 +1,19 @@
 (function () {
-
     let chatButton = document.getElementById("chat-button");
-
     chatButton.addEventListener("click", () => {
+        let botMessage = "";
         let chatInput = document.getElementById("chat-input");
-
         let chatLog = document.getElementById("chat-log");
-
         let key = document.getElementById("key").value
+        let userMessage = chatInput.value;
+        if (!key) {
+            botMessage = "不填key有啥好聊的"
+            chatLog.innerHTML +=
+                `<div class="user-message">${userMessage}</div>` +
+                `<div class="bot-message">${botMessage}</div>`;
+            chatInput.value = "";
+            return
+        }
         let model = document.getElementById("model").value
         let max_tokens = document.getElementById("max_tokens").value
         let temperature = document.getElementById("temperature").value
@@ -15,8 +21,6 @@
         let presence_penalty = document.getElementById("presence_penalty").value
         let frequency_penalty = document.getElementById("frequency_penalty").value
         let port = document.getElementById("port").value
-        let userMessage = chatInput.value;
-        let botMessage = "";
         let apiCompletion = async (text) => {
             let res = await axios.post(`http://127.0.0.1:${port}/completions`, {
                 key: key,
@@ -30,24 +34,29 @@
             });
             return res;
         };
-        chatButton.style.display = "none";
+        chatButton.textContent = "等待返回";
         apiCompletion(userMessage).then(function (response) {
-            botMessage = response.data.choices[0].text
+            try{
+                botMessage = response.data.choices[0].text
+            }catch (e) {
+                botMessage = response.data.error.message
+            }
             chatLog.innerHTML +=
                 `<div class="user-message">${userMessage}</div>` +
                 `<div class="bot-message">${botMessage}</div>`;
             chatInput.value = "";
-            chatButton.style.display = "";
+            chatButton.textContent = "发送";
+
         }).catch(function (error) {
             botMessage = error
             chatLog.innerHTML +=
                 `<div class="user-message">${userMessage}</div>` +
                 `<div class="bot-message">${botMessage}</div>`;
             chatInput.value = "";
-            chatButton.style.display = "";
+            chatButton.textContent = "发送";
+
         })
     });
-
     document.getElementById("clear-button").addEventListener("click", function () {
         document.getElementById("chat-log").innerHTML = "";
     });
